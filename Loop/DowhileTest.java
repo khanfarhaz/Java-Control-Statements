955bbaa5dcfbe0f8c2e36fbf4ffeae341ebe4ca0
khanfarhaz <92144622+khanfarhaz@users.noreply.github.com>
2021-11-23 11:42:55 +0530

    Add files via upload

---
+++ Loop/DowhileTest.java
@@ -0,0 +1,16 @@
+package Loop;
+
+public class DowhileTest {
+	 public static void main(String[] args) {
+	 
+	        int i=10;
+	        do
+	        {
+	            System.out.print(" "+i);
+	            i--;
+	        }
+	        while(i>0);
+	    }
+	}
+
+
---
+++ Loop/ForEachTest.java
@@ -0,0 +1,12 @@
+package Loop;
+
+public class ForEachTest {
+ public static void main(String args[])
+ {
+		   int a[] = {10,15,20,25,30};
+		    for (int i : a) {
+		        System.out.println(i);
+		    }
+		 }
+
+}
